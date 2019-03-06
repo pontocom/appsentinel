fca0c783e9b2dd319a8f83d0d9de146ea3d625bc
Carlos Serrao <carlos.j.serrao@gmail.com>
2019-03-06 20:02:13 +0000

    Started working on the plugin staructure for our tool!

--- plugins/plugin_DroidStatX.py
+++ plugins/plugin_DroidStatX.py
@@ -1,1 +1,8 @@
-#Plugin to handle the tools capable of setting the input for "DroidstatX" and handle the output
+# Plugin to handle the tools capable of setting the input for "DroidstatX" and handle the output
+modName = "DroidStatX"
+enable = True
+
+
+class PluginClass:
+    def run(self):
+        print("Running the DroidStatX plugin!...")
--- vulnScanManager.py
+++ vulnScanManager.py
@@ -1,9 +1,45 @@
-#vulnScanManager, will be responsible for checking a specific directory for apks and for processing them!
+# vulnScanManager, will be responsible for checking a specific directory for apks and for processing them!
 import os.path
+import argparse
 
-pluginDir = os.path.dirname(os.path.abspath(__file__))
-print(pluginDir)
+if __name__=="__main__":
+    VERSION = '0.1'
+    banner = """
+                .__           _________                        _____                                             
+    ___  ____ __|  |   ____  /   _____/ ____ _____    ____    /     \ _____    ____ _____     ____   ___________ 
+    \  \/ /  |  \  |  /    \ \_____  \_/ ___\\\\__  \  /    \  /  \ /  \\\\__  \  /    \\\\__  \   / ___\_/ __ \_  __ \ 
+     \   /|  |  /  |_|   |  \/        \  \___ / __ \|   |  \/    Y    \/ __ \|   |  \/ __ \_/ /_/  >  ___/|  | \/ 
+      \_/ |____/|____/___|  /_______  /\___  >____  /___|  /\____|__  (____  /___|  (____  /\___  / \___  >__|   
+                          \/        \/     \/     \/     \/         \/     \/     \/     \//_____/      \/       """
 
-for file in os.listdir(pluginDir + "/plugins"):
-    if file[0:7] == "plugin_" and file[-3:] == ".py":
-        print(file)
+    plugins = []
+    thisPlugin = 0
+
+    print(str(banner))
+
+    text = "Tool that scans APKs and looks for vulnerabilities"
+    parser = argparse.ArgumentParser(description = text)
+    parser.add_argument('-v', '--version', action='version', version='Vulnerability Scan Manager ' + VERSION)
+    parser.add_argument('-d', '--directory', help='Location to the directory that contains APKs to analyse.', action='store', dest='apkdir', nargs=1, default='./')
+    parser.add_argument('-p', '--plugins', help='Location of the plugins directory.',
+                        action='store', dest='plugdir', nargs=1, default='./')
+    args = parser.parse_args()
+
+    print(args)
+
+    # looking for the plugins
+    pluginDir = os.path.dirname(os.path.abspath(__file__))
+    print(pluginDir)
+
+    for file in os.listdir(pluginDir + "/plugins"):
+        if file[0:7] == "plugin_" and file[-3:] == ".py":
+            print(file)
+            # we need to do something with them... need to check if it is beter ti import or to spawn
+            print("Importing -> " + ".".join(file.split(".")[0:-1]))
+            thisPlugin = __import__("plugins." + ".".join(file.split(".")[0:-1]))
+            if thisPlugin.enable == True:
+                plugins.append(thisPlugin)
+
+    # looking for the APKs to analyse
+    # we use the same approach to look for the APKs to analyse
+
