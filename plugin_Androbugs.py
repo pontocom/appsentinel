5277b8faca83bc00d0d138d25fb968c7a606bb5d
Carlos Serrao <carlos.j.serrao@gmail.com>
2019-03-06 22:43:21 +0000

    Added another plugin to test. Added some extra functions... still not convinced of this approach for the problem!

--- plugin_Androbugs.py
+++ plugin_Androbugs.py
@@ -1,4 +1,6 @@
 # Plugin to handle the tools capable of setting the input for "DroidstatX" and handle the output
+import os
+
 pluginName = "Androbugs"
 enable = True
 
@@ -6,3 +8,5 @@ enable = True
 class PluginClass:
     def run(self):
         print("Running the Androbugs plugin!...")
+        os.system("python2 ../droidstatx/droidstatx.py --apk ")
+
