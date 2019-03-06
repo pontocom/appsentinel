8db317cda6c968dc428ee825a4e2f8b709e89eeb
Carlos Serrao <carlos.j.serrao@gmail.com>
2019-03-06 18:06:36 +0000

    Started working on the plugin staructure for our tool!

--- vulnScanManager.py
+++ vulnScanManager.py
@@ -1,1 +1,9 @@
 #vulnScanManager, will be responsible for checking a specific directory for apks and for processing them!
+import os.path
+
+pluginDir = os.path.dirname(os.path.abspath(__file__))
+print(pluginDir)
+
+for file in os.listdir(pluginDir + "/plugins"):
+    if file[0:7] == "plugin_" and file[-3:] == ".py":
+        print(file)
