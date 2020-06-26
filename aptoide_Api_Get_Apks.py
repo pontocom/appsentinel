54373071b7a0048dd5845987dc8b5ca7af68b551
pontocom <carlos.j.serrao@gmail.com>
2020-06-26 17:52:41 +0100

    Changes to clean and uniform some stuff - after merging tests with master!!!

--- aptoide_Api_Get_Apks.py
+++ aptoide_Api_Get_Apks.py
@@ -3,7 +3,7 @@ import json
 import manager
 
 
-urlApsList = 'http://ws75.aptoide.com/api/7/apps/get/store_name=apps/limit='
+urlApsList = 'http://ws75.aptoide.com/api/7/apps/get/sort=downloads/order=DESC/limit='
 
 APPS_PER_GROUP = 1
 
@@ -16,16 +16,18 @@ dataResult=[]
 with open('aptoideGroups.txt', 'r') as f:
     allGroups = f.readlines()
     for group in allGroups:
-        print('antes')
+        print('[' + group.rstript() + ']')
+        print('Requesting ['+ urlApsList + str(APPS_PER_GROUP) +'/group_name=' + group +']')
         response = requests.get(urlApsList + str(APPS_PER_GROUP) +'/group_name=' + group)
         content = response.json()
-        #print(content)
         try:
             for i in range(0,APPS_PER_GROUP):
+                name = content['datalist']['list'][i]['name']
+                package = content['datalist']['list'][i]['package']
                 md5 = content['datalist']['list'][i]['file']['md5sum']
                 size = content['datalist']['list'][i]['file']['filesize']
                 data = manager.get_json_data(md5)
-                print(md5)
+                print("[" + str(totalApps+1) + "][" + md5 + "][" + name + "][" + package + "]")
                 if (data['info']['status'] != 'FAIL'):
                     manager.download_apk(data['nodes']['meta']['data']['file']['path'])
                     dataResult.append({
